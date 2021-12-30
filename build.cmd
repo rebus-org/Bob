84f9d356cff8e395aaac449cfae640d5625cf7a5
Mogens Heller Grabe <mookid8000@gmail.com>
2021-12-30 09:25:14 +0100

    fix build script

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 @echo off
 
-dotnet publish Bob -c Release -r win-x64 --self-contained -p:PublishSingleFile=true;PublishTrimmed=true;PublishReadyToRun=true;NuGetInteractive=true
+dotnet restore Bob --interactive
+dotnet publish Bob -c Release --no-restore -r win-x64 --self-contained -p:PublishSingleFile=true;PublishTrimmed=true
